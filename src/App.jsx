f65081fcc2a7b45070a5b01dbecab6c3c53ff606
Arto Pöllänen <arto.pollanen@iki.fi>
2025-10-05 10:41:10 +0300

    update su 05.10.2025 10.41.10,19

--- src/App.jsx
+++ src/App.jsx
@@ -217,7 +217,7 @@ export default function App() {
 
           <div style={titleRowCentered}>
             <div style={titleStyle}>Lyriikkarenki</div>
-            <div style={versionInline}>v0.8</div>
+            <div style={versionInline}>v0.8 dev</div>
           </div>
 
           <button
